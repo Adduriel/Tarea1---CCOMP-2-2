389793ca17d12ddd017a5f2f80d572badc8c99e5
Adduriel <78422565+Adduriel@users.noreply.github.com>
2021-09-02 18:51:01 -0500

    Update 29.cpp

--- 29.cpp
+++ 29.cpp
@@ -3,26 +3,7 @@
 using namespace std;
 
 int main(){
-    int cara_largo;
-    cout<<"Introduce el largo de la cara(0-4): ";
-    cin>>cara_largo;
-
-    if(cara_largo == 0){
-        cout<<"La cara superficial es 0 y el volumen es 0\n";
-    }
-    else if(cara_largo == 1){
-        cout<<"La cara superficial es 6 y el volumen es 1\n";
-    }
-    else if(cara_largo == 2){
-        cout<<"La cara superficial es 24 y el volumen es 8\n";
-    }
-    else if(cara_largo == 3){
-        cout<<"La cara superficial es 54 y el volumen es 27\n";
-    }
-    else if(cara_largo == 4){
-        cout<<"La cara superficial es 96 y el volumen es 64\n";
-    }
-    else{
-        cout<<"Introduce un valor en el rango\n";
-    }
+    cout<<"Cara larga\ndel cubo(cm)\n \n0\n1\n2\n3\n4\n";
+    cout<<"Area de la\nsuperficie del cubo(cm2)\n \n0\n6\n24\n54\n96\n";
+    cout<<"Volumen\ndel cubo(cm3)\n \n0\n1\n8\n27\n64\n";
 }
