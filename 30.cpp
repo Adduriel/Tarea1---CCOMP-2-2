e5ceeea5b3ddff136a44ed76cfea4f9a551e1ef3
Adduriel <78422565+Adduriel@users.noreply.github.com>
2021-09-02 18:51:25 -0500

    Update 30.cpp

--- 30.cpp
+++ 30.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(){
     int pesok, alturam;
-    cout<<"ingrese los datos para la calculadora de masa: ";
+    cout<<"ingrese los datos para la calculadora de masa:\nPeso en kilos y altura en metros:";
     cin>>pesok>>alturam;
 
     int BMI = pesok/alturam*alturam;
 
      if(BMI <= 18){
-         if else(BMI >= 18 && BMI <= 25){
-                if else(BMI >= 25 && BMI <= 30){
-                         if else(BMI > 30){
+         if(BMI >= 18 && BMI <= 25){
+                if(BMI >= 25 && BMI <= 30){
+                         if(BMI > 30){
                              cout<<"Usted esta obeso";
                             }
                     cout<<"Usted esta con sobrepeso";
